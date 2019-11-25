6b101191320e3e790c64fdf9a3810693496fb979
MonikaSarwaiya <56624084+MonikaSarwaiya@users.noreply.github.com>
2019-11-25 13:24:05 +0530

    Hibernate

---
+++ Hibernate/jpawithhibernate/src/main/java/com/ustglobal/jpawithhibernateapp/jpql/DynamicUpdateDemo.java
@@ -0,0 +1,36 @@
+package com.ustglobal.jpawithhibernateapp.jpql;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+public class DynamicUpdateDemo {
+
+public static void main(String[] args) {
+		
+		EntityManager entityManager = null;
+		EntityTransaction transaction  = null;
+		
+		try {
+			EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("TestPersistence");
+			entityManager = entityManagerFactory.createEntityManager();
+			transaction = entityManager.getTransaction();
+			transaction.begin();
+			String jpql = "update Product set pname=:name where pid=:id";
+			Query query = entityManager.createQuery(jpql);
+			query.setParameter("name", "Book");
+			query.setParameter("id", 1);
+			int result = query.executeUpdate();
+			System.out.println(result);
+			transaction.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			transaction.rollback();
+		}finally {
+		entityManager.close();
+	}
+	}
+
+}
---
+++ Hibernate/jpawithhibernateapp/src/main/java/com/ustglobal/jpawithhibernateapp/ReattachingDemo.java
@@ -0,0 +1,39 @@
+package com.ustglobal.jpawithhibernateapp;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+import com.ustglobal.jpawithhibernateapp.dto.Product;
+
+public class ReattachingDemo {
+
+	public static void main(String[] args) {
+
+		EntityManager entityManager = null;
+		EntityTransaction entityTransaction = null;
+		
+		try {
+			EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("TestPersistence");
+			entityManager = entityManagerFactory.createEntityManager();
+			entityTransaction = entityManager.getTransaction();
+			entityTransaction.begin();
+			Product prod = entityManager.find(Product.class, 1);
+			System.out.println(entityManager.contains(prod));
+			entityManager.detach(prod);
+			System.out.println(entityManager.contains(prod));
+            Product prod2 = entityManager.merge(prod);
+			prod2.setPname("Laptop");
+			System.out.println("Record Updated");
+			entityTransaction.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			entityTransaction.rollback();
+		}
+		entityManager.close();
+		
+		
+	}
+
+}
